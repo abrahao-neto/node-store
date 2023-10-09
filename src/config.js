922ed1d0485654960a14248c7d1cc621551feac8
abrahao-neto <abrahao-neto@outlook.com>
2023-10-09 00:18:06 -0300

    .

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
 global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo a Node Store!';
 
 module.exports = {
-    connectionString: 'mongodb+srv://abrahao:jesus2023@nodestrbd.uikyudj.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp',
+    connectionString: 'mongodb',
     sendgridkey: 'SUA CHAVE',
     containerConnectionString: 'TDB'
 }
